b91640c2b26beb0c8108280e1e68c8c83372999c
JonLedo <jonledo.code@gmail.com>
2023-03-16 19:52:41 -0400

    connecetd mongo atlas DB to the server and set up env variables

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
+
 // express
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000
 
+// database
+const connectDB = require('./db/connect')
+
+// start the server
+const PORT = process.env.PORT || 5000
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL)
+
     app.listen(PORT, console.log('Server is listening on ' + PORT))
   } catch (error) {
     console.log(error)
