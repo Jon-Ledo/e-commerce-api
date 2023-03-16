7052c2d715f463b0b56427e1e7859675b6bd3148
JonLedo <jonledo.code@gmail.com>
2023-03-16 19:17:15 -0400

    base express set up just to get server running

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('E-Commerce API');
+// express
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    app.listen(PORT, console.log('Server is listening on ' + PORT))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
