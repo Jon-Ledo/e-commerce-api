a34d0cd738981d1a64577b07b096b4e8408d0ee7
JonLedo <jonledo.code@gmail.com>
2023-03-22 11:17:04 -0400

    completed the showmeuser route, that sends a response of the curfrently logged in user. Appllied the authenticate middleware into the route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,7 @@ router
   .route('/')
   .get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers)
 
-router.route('/showMe').get(showCurrentUser)
+router.route('/showMe').get(authenticateUser, showCurrentUser)
 router.route('/updateUser').patch(updateUser)
 router.route('/updateUserPassword').patch(updateUserPassword)
 
