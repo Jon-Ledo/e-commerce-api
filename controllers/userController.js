ebfb7502b7c97a1f5fce386f2325510f186c7e1d
JonLedo <jonledo.code@gmail.com>
2023-03-22 11:52:59 -0400

    added authenticateUser to the route, and set up the route to allow a user's password to be changed after passing validationn checks

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,7 +30,22 @@ const updateUser = async (req, res) => {
 }
 
 const updateUserPassword = async (req, res) => {
-  res.send('update user password')
+  const { oldPassword, newPassword } = req.body
+
+  if (!oldPassword || !newPassword) {
+    throw new customError.BadRequestError('Please provide both values')
+  }
+
+  const user = await User.findOne({ _id: req.user.userId })
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword)
+  if (!isPasswordCorrect) {
+    throw new customError.UnauthenticatedError('Invalid credentials')
+  }
+
+  user.password = newPassword
+  await user.save()
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password updated' })
 }
 
 module.exports = {
