2cc26567fb1b9d8034ffbb45b7d2899afc8189be
JonLedo <jonledo.code@gmail.com>
2023-03-23 17:34:34 -0400

    set up the product schema

---
+++ models/Product.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide product name'],
+      trim: true,
+      minlength: [3, 'Name can not be shoret than 3 characters'],
+      maxlength: [100, 'Name can not be longer than 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      maxlength: [1000, 'Description can not be loner than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
