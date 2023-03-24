664caff5cd5ebdc0aa81563ef72f3ec91a7e5fcf
JonLedo <jonledo.code@gmail.com>
2023-03-24 00:12:25 -0400

    set up the image upload controller. It uses the express-file-upload package, and stores the images onto the server. left comments inside code to rememebr for future use. included the exmaple image as well, as a default

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const path = require('path')
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId
@@ -58,8 +59,39 @@ const deleteProduct = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Success! Product removed!' })
 }
 
+// NOTE: This method saves uploaded images onto the server
 const uploadImage = async (req, res) => {
-  res.send('image')
+  // console.log(req.files)
+
+  // After installing packages and setting up the static middleware
+  // check if the file is uploaded
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
+
+  // store image file data into variable
+  const productImage = req.files.image
+  // check if the file is actually an image mimetype
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please upload image')
+  }
+
+  // check for image size
+  const maxSize = 1024 * 1024
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1MB'
+    )
+  }
+
+  // move the image using mv function
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  )
+  await productImage.mv(imagePath)
+
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` })
 }
 
 module.exports = {
