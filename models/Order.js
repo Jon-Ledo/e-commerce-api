74e54689626e901a01db157385e43a61b63075a5
JonLedo <jonledo.code@gmail.com>
2023-03-25 23:21:39 -0400

    set up order schemas to be used

---
+++ models/Order.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+const SingleCartItemSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+})
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: {
+      enum: [SingleCartItemSchema],
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = model('Order', OrderSchema)
